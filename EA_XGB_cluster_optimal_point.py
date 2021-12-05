4cae6545132338d3c4cf6fe86411da34d4b766e2
Buwei-He <70881012+Buwei-He@users.noreply.github.com>
2021-12-05 23:02:47 +0800

    Update EA_XGB_cluster_optimal_point.py

--- EA_XGB_cluster_optimal_point.py
+++ EA_XGB_cluster_optimal_point.py
@@ -28,7 +28,7 @@ seed = int(np.random.rand(1) * 1e5)
 print(seed)
 LHS = True
 
-parameters = {'booster': 'gblinear', 'seed': 100, 'nthread': -1, 'gamma': 0, 'lambda': 6,
+parameters = {'booster': 'gbtree', 'seed': 100, 'nthread': -1, 'gamma': 0, 'lambda': 6,
                   'max_depth': 6, 'eta': 0.15, 'objective': 'reg:squarederror'}
 num_boost_round = 250
 
@@ -137,7 +137,7 @@ def popEvaluate(population):
         individual.fitness.values = np.array(pop_pred[i]).ravel()
     return pop_array, pop_pred
 
-def sampleSelect(candidate_population, sample_num):
+def sampleSelect(candidate_population, sample_Train):
     """
     从现存种群中选出下一个采样点，本函数和贝叶斯优化的采集函数具有类似的功能，需要对贪心和探索进行平衡。
     目前的思路是随着训练模型的样本量逐渐增加，选择更相信模型预测结果。样本量小时在前5名
@@ -152,40 +152,44 @@ def sampleSelect(candidate_population, sample_num):
     pop_array, pop_pred = popEvaluate(pop)
     POP = np.hstack((pop_array, pop_pred.reshape(-1, 1)))  # 适应度和特征拼起来的array
 
+    sample_num = len(sample_Train)
+
     if sample_num < threshold[0]:
-        sample_select = np.empty((0, 5))
-        Clusters = []
         cluster = num_cluster[0]
-        kms = KMeans(init='k-means++', n_clusters=cluster, random_state=1, tol=1e-3)
-        pred = kms.fit_predict(POP)
-        for i in range(cluster):
-            c = POP[pred[0:] == i, 0:6]
-            Clusters.append(c)
-        for i in range(cluster):
-            C = Clusters[i]
-            index = np.argsort(C[:, 5])
-            C = C[index, :]
-            select = C[0, 0:5]
-            select.reshape(1, -1)
-            sample_select = np.vstack((sample_select, select))
-        return sample_select
-    elif sample_num < threshold[1]:
-        sample_select = np.empty((0, 5))
-        Clusters = []
+    else:
         cluster = num_cluster[1]
-        kms = KMeans(init='k-means++', n_clusters=cluster, random_state=1, tol=1e-3)
-        pred = kms.fit_predict(POP)
-        for i in range(cluster):
-            c = POP[pred[0:] == i, 0:6]
-            Clusters.append(c)
-        for i in range(cluster):
-            C = Clusters[i]
-            index = np.argsort(C[:, 5])
-            C = C[index, :]
-            select = C[0, 0:5]
-            select.reshape(1, -1)
-            sample_select = np.vstack((sample_select, select))
-        return sample_select
+    sample_select = np.empty((0, 5))
+    Clusters = []
+
+    kms = KMeans(init='k-means++', n_clusters=cluster, random_state=1, tol=1e-3)
+    pred = kms.fit_predict(POP)
+    for i in range(cluster):
+        c = POP[pred[0:] == i, :]
+        Clusters.append(c)
+    for i in range(cluster):
+        C = Clusters[i]
+        C = C[np.argsort(C[:, -1]), :]
+        select = C[0, :-1]
+
+        # 样本点去重：如果重复则选择次优的点
+        default_select = 0
+        # equal_mat = sample_Train - select
+        # equal_res = np.any(equal_mat, axis=1)
+        # e = equal_res.all()
+
+        while ~np.any(sample_Train - select, axis=1).all():
+            default_select += 1
+            if default_select < C.shape[0]:
+                select = C[default_select, :-1]
+            else:
+                print("Too many repeated sample attempts, fail to select a new point.")
+                select = C[0, :-1]
+                break
+
+        select.reshape(1, -1)
+        sample_select = np.vstack((sample_select, select))
+    return sample_select
+
 
 
 
@@ -235,7 +239,7 @@ def resultDisp(Sample_Points, Sample_Init, generations, Data):
     global computations
     global sample_init
     g = range(computations - sample_init)
-    plt.figure(figsize=(20, 10))
+    plt.figure(figsize=(10.24, 7.68))
     plt.xlabel('Generations')
     plt.ylabel('Sample Values')
     plt.legend('EA-XGB', loc='lower right')
@@ -243,7 +247,7 @@ def resultDisp(Sample_Points, Sample_Init, generations, Data):
     plt.plot(g, SampleValues, 'r-', lw=1)
     plt.scatter(g, SampleValues, alpha=1)
 
-    # plt.figure(figsize=(20, 10))
+    # plt.figure(figsize=(10.24, 7.68))
     # plt.xlabel('Generations')
     # plt.ylabel('BestWrapRate')
     plt.legend('EA-XGB', loc='lower right')
@@ -288,7 +292,7 @@ def iterate(Sample_Train, Sample_Points, Data, num_boost_round):
         # 采样
         # 选择种群中的最佳点作为下一个CAE仿真采样点
         # 大坑注意：Best是选最小，Worst是选最大(因为初始化时优化问题设置的是最小为优)
-        sample = sampleSelect(pop, len(Sample_Train))  # 选择下一个采样点
+        sample = sampleSelect(pop, Sample_Train)  # 选择下一个采样点
         Sample_Points = np.vstack((Sample_Points, sample))
         Sample_Train = np.vstack((Sample_Train, sample))
         # 训练
